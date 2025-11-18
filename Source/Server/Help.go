75b7ea55735fa1c770cb01a6487c6b1ca53d6cc2
Khorlane <khorlane@yahoo.com>
2025-11-17 21:59:43 -0500

    Help.go is done

--- Source/Server/Help.go
+++ Source/Server/Help.go
@@ -1,7 +1,76 @@
 package server
 
-// Placeholder for Help-related functions
+import (
+	"bufio"
+	"os"
+)
+
+var HelpFile *os.File
+var HelpScanner *bufio.Scanner
+var HelpText string
+
 func IsHelp() bool {
-  // Placeholder for checking if a topic is a help topic
-  return false
+	DEBUGIT(1)
+	Found := false
+	if !OpenFile() {
+		return false
+	}
+	HelpLookup := StrGetWord(CmdStr, 2)
+	HelpLookup = StrMakeLower(HelpLookup)
+	HelpText = "Not Done"
+	for HelpText != "End of Help" {
+		ReadLine()
+		HelpText = StrTrimLeft(HelpText)
+		TmpStr = StrLeft(HelpText, 5)
+		if TmpStr == "Help:" {
+			TmpStr = StrRight(HelpText, StrGetLength(HelpText)-5)
+			TmpStr = StrMakeLower(TmpStr)
+			if TmpStr == HelpLookup {
+				Found = true
+				ShowHelp()
+				HelpText = "End of Help"
+			}
+		}
+	}
+	if HelpFile != nil {
+		HelpFile.Close()
+	}
+	if Found {
+		return true
+	}
+	return false
+}
+
+func OpenFile() bool {
+	DEBUGIT(1)
+	HelpFileName := HELP_DIR + "Help.txt"
+	file, err := os.Open(HelpFileName)
+	if err != nil {
+		return false
+	}
+	HelpFile = file
+	HelpScanner = bufio.NewScanner(HelpFile)
+	return true
+}
+
+func ReadLine() {
+	DEBUGIT(1)
+	if HelpScanner != nil && HelpScanner.Scan() {
+		HelpText = HelpScanner.Text()
+	} else {
+		HelpText = ""
+	}
+}
+
+func ShowHelp() {
+	TmpStr = StrLeft(HelpText, 13)
+	for TmpStr != "Related help:" {
+		ReadLine()
+		pDnodeActor.PlayerOut += HelpText
+		pDnodeActor.PlayerOut += "\r\n"
+		TmpStr = StrLeft(HelpText, 13)
+	}
+	pDnodeActor.PlayerOut += "\r\n"
+	CreatePrompt(pDnodeActor.pPlayer)
+	pDnodeActor.PlayerOut += GetPlayerOutput(pDnodeActor.pPlayer)
 }
