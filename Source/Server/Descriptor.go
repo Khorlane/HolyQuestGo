3a2b7f70afac46852392087eda0d57a4130f0caf
Khorlane <khorlane@yahoo.com>
2025-11-28 12:22:48 -0500

    Descriptor.go - done

--- Source/Server/Descriptor.go
+++ Source/Server/Descriptor.go
@@ -1,13 +1,67 @@
 package server
 
-type Descriptor struct {
-	// Placeholder for descriptor fields
+import "os"
+
+// Append new connection to connection list
+func AppendIt() {
+  pDnodeActor.pDnodePrev = pDnodeHead.pDnodePrev
+  pDnodeHead.pDnodePrev.pDnodeNext = pDnodeActor
+  pDnodeHead.pDnodePrev = pDnodeActor
+  pDnodeActor.pDnodeNext = pDnodeHead
 }
 
-func InitDescriptor() {
-	// Placeholder for initializing descriptors
-}
-
+// Clear descriptor linked list
 func ClearDescriptor() {
-	// Placeholder for clearing descriptors
+  DnodeDestructor(pDnodeHead)
+  pDnodeHead = nil
+}
+
+// Delete connection from connection list and close socket
+func DeleteNode() bool {
+  if pDnodeCursor.DnodeFd == 0 {
+    return false
+  }
+  Result := CloseSocket(pDnodeCursor.DnodeFd)
+  if Result != 0 {
+    PrintIt("Descriptor::DeleteNode - Error: Closesocket")
+    os.Exit(1)
+  }
+  pDnodeActor = nil
+  pDnode := pDnodeCursor.pDnodePrev
+  DnodeDestructor(pDnodeCursor)
+  pDnodeCursor = pDnode
+  return true
+}
+
+// End of Dnode list?
+func EndOfDnodeList() bool {
+  return pDnodeCursor.DnodeFd == 0
+}
+
+// Get Dnode pointer
+func GetDnode() *Dnode {
+  return pDnodeCursor
+}
+
+// Initialize descriptor pointers
+func InitDescriptor() {
+  pDnodeHead = DnodeConstructor(0, "")
+  pDnodeHead.pDnodeNext = pDnodeHead
+  pDnodeHead.pDnodePrev = pDnodeHead
+  pDnodeCursor = pDnodeHead
+}
+
+// Set Dnode pointer to the first Dnode in the list
+func SetpDnodeCursorFirst() {
+  pDnodeCursor = pDnodeHead.pDnodeNext
+}
+
+// Set Dnode pointer to the next Dnode in the list
+func SetpDnodeCursorNext() {
+  pDnodeCursor = pDnodeCursor.pDnodeNext
+}
+
+// Set Dnode pointer to the previous Dnode in the list
+func SetpDnodeCursorPrev() {
+  pDnodeCursor = pDnodeCursor.pDnodePrev
 }
