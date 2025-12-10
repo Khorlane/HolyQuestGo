9ec26d2502cb22ea41002cb77f6bf06443683bfb
Khorlane <khorlane@yahoo.com>
2025-12-10 13:59:21 -0500

    Add stub functions to Room.go

--- Source/Server/Room.go
+++ Source/Server/Room.go
@@ -9,4 +9,62 @@
 
 package server
 
-// Placeholder for Room-related functions
+func GetRoomId(RoomId string) string {
+	return ""
+}
+
+func GetRoomName(RoomId string) string {
+	return ""
+}
+
+func GetValidMobRoomExits(RoomId string) string {
+	return ""
+}
+
+func IsExit(MudCmdIsExit string) bool {
+	return false
+}
+
+func IsRoom(RoomId string) bool {
+	return false
+}
+
+func IsRoomType(RoomId string, RoomType string) bool {
+	return false
+}
+
+func ShowRoom(pDnode *Dnode) {
+	// TODO: Implement ShowRoom logic
+}
+
+func CloseFile() {
+	// TODO: Implement CloseFile logic
+}
+
+func MoveFollowers(pDnode *Dnode, ExitToRoomId string) {
+	// TODO: Implement MoveFollowers logic
+}
+
+func MovePlayer(pDnode *Dnode, ExitToRoomId string) {
+	// TODO: Implement MovePlayer logic
+}
+
+func OpenFile(pDnode *Dnode) bool {
+	return false
+}
+
+func ShowRoomDesc(pDnode *Dnode) {
+	// TODO: Implement ShowRoomDesc logic
+}
+
+func ShowRoomExitDesc() {
+	// TODO: Implement ShowRoomExitDesc logic
+}
+
+func ShowRoomExits(pDnode *Dnode) {
+	// TODO: Implement ShowRoomExits logic
+}
+
+func ShowRoomName(pDnode *Dnode) {
+	// TODO: Implement ShowRoomName logic
+}
