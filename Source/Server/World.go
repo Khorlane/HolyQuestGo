670b910a9df9532ebaf8c9509e632d615a6c651f
Khorlane <khorlane@yahoo.com>
2025-12-10 14:11:02 -0500

    Remove blank lines from World.go

--- Source/Server/World.go
+++ Source/Server/World.go
@@ -23,12 +23,10 @@ import (
 // Check 'spawn mobile' events
 func CheckSpawnMobileEvents() {
 	CheckTime := fmt.Sprintf("%d", GetTimeSeconds())
-
 	if err := ChgDir(CONTROL_EVENTS_DIR); err != nil {
 		LogIt("CheckSpawnMobileEvents - Change directory to CONTROL_EVENTS_DIR failed")
 		os.Exit(1)
 	}
-
 	err := filepath.Walk("./", func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
@@ -36,18 +34,15 @@ func CheckSpawnMobileEvents() {
 		if info.IsDir() {
 			return nil
 		}
-
 		EventFileName := info.Name()
 		if !strings.HasPrefix(EventFileName, "M") {
 			return nil
 		}
-
 		EventTime := strings.TrimSuffix(EventFileName, filepath.Ext(EventFileName))
 		EventTime = EventTime[1:] // Remove the 'M' prefix
 		if EventTime > CheckTime {
 			return nil
 		}
-
 		EventFilePath := CONTROL_EVENTS_DIR + EventFileName
 		file, err := os.Open(EventFilePath)
 		if err != nil {
@@ -55,28 +50,23 @@ func CheckSpawnMobileEvents() {
 			os.Exit(1)
 		}
 		defer file.Close()
-
 		var Stuff string
 		for fmt.Fscanln(file, &Stuff); Stuff != ""; fmt.Fscanln(file, &Stuff) {
 			MobileID := strings.Fields(Stuff)[0]
 			RoomID := strings.Fields(Stuff)[1]
 			SpawnMobile(MobileID, RoomID)
 		}
-
 		file.Close()
 		if err := os.Remove(EventFilePath); err != nil {
 			LogIt("CheckSpawnMobileEvents - Remove Events file failed")
 			os.Exit(1)
 		}
-
 		return nil
 	})
-
 	if err != nil {
 		LogIt("CheckSpawnMobileEvents - Error walking through files")
 		os.Exit(1)
 	}
-
 	if err := ChgDir(HomeDir); err != nil {
 		LogIt("CheckSpawnMobileEvents - Change directory to HomeDir failed")
 		os.Exit(1)
@@ -89,7 +79,6 @@ func CreateSpawnMobileEvents() {
 		LogIt("CreateSpawnMobileEvents - Change directory to WORLD_MOBILES_DIR failed")
 		os.Exit(1)
 	}
-
 	err := filepath.Walk("./", func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
@@ -97,18 +86,15 @@ func CreateSpawnMobileEvents() {
 		if info.IsDir() {
 			return nil
 		}
-
 		WorldMobileFileName := info.Name()
 		MobileID := strings.TrimSuffix(WorldMobileFileName, filepath.Ext(WorldMobileFileName))
 		if MobileID == "ReadMe" {
 			return nil
 		}
-
 		ControlMobSpawnFileName := CONTROL_MOB_SPAWN_DIR + MobileID
 		if _, err := os.Stat(ControlMobSpawnFileName); err == nil {
 			return nil
 		}
-
 		WorldMobileFilePath := WORLD_MOBILES_DIR + WorldMobileFileName
 		file, err := os.Open(WorldMobileFilePath)
 		if err != nil {
@@ -116,29 +102,24 @@ func CreateSpawnMobileEvents() {
 			os.Exit(1)
 		}
 		defer file.Close()
-
 		var Stuff string
 		fmt.Fscanln(file, &Stuff)
 		if !strings.HasPrefix(Stuff, "MaxInWorld:") {
 			LogIt("CreateSpawnMobileEvents - World mobile file format error MaxInWorld")
 			os.Exit(1)
 		}
-
 		// Placeholder for CountMob and additional logic
 		return nil
 	})
-
 	if err != nil {
 		LogIt("CreateSpawnMobileEvents - Error walking through files")
 		os.Exit(1)
 	}
-
 	if err := ChgDir(HomeDir); err != nil {
 		LogIt("CreateSpawnMobileEvents - Change directory to HomeDir failed")
 		os.Exit(1)
 	}
 }
-
 // Handle world events
 func Events() {
 	CreateSpawnMobileEvents()
@@ -152,7 +133,6 @@ func HealMobiles() {
 		LogIt("HealMobiles - Change directory to MOB_STATS_HPT_DIR failed")
 		os.Exit(1)
 	}
-
 	// Get a list of all MobStats\HitPoints files
 	err := filepath.Walk("./", func(path string, info os.FileInfo, err error) error {
 		if err != nil {
@@ -161,14 +141,11 @@ func HealMobiles() {
 		if info.IsDir() {
 			return nil
 		}
-
 		MobStatsHitPointsFileName := info.Name()
 		MobileID := strings.TrimSuffix(MobStatsHitPointsFileName, filepath.Ext(MobStatsHitPointsFileName))
-
 		if HealMobilesFightCheck("MobPlayer", MobileID) || HealMobilesFightCheck("PlayerMob", MobileID) {
 			return nil
 		}
-
 		RoomID := GetMobileRoom(MobileID)
 		RemoveMobFromRoom(RoomID, MobileID)
 		DeleteMobStats(MobileID)
@@ -179,12 +156,10 @@ func HealMobiles() {
 		AddMobToRoom(RoomID, MobileID)
 		return nil
 	})
-
 	if err != nil {
 		LogIt("HealMobiles - Error walking through files")
 		os.Exit(1)
 	}
-
 	if err := ChgDir(HomeDir); err != nil {
 		LogIt("HealMobiles - Change directory to HomeDir failed")
 		os.Exit(1)
@@ -194,7 +169,6 @@ func HealMobiles() {
 // See if mobile is fighting
 func HealMobilesFightCheck(dir, mobileID string) bool {
 	MobFighting := false
-
 	switch dir {
 		case "MobPlayer":
 			if err := ChgDir(MOB_PLAYER_DIR); err != nil {
@@ -207,7 +181,6 @@ func HealMobilesFightCheck(dir, mobileID string) bool {
 				os.Exit(1)
 			}
 	}
-
 	err := filepath.Walk("./", func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
@@ -215,7 +188,6 @@ func HealMobilesFightCheck(dir, mobileID string) bool {
 		if info.IsDir() {
 			return nil
 		}
-
 		MobPlayerFileName := info.Name()
 		file, err := os.Open(MobPlayerFileName)
 		if err != nil {
@@ -223,7 +195,6 @@ func HealMobilesFightCheck(dir, mobileID string) bool {
 			os.Exit(1)
 		}
 		defer file.Close()
-
 		var Stuff string
 		for fmt.Fscanln(file, &Stuff); Stuff != ""; fmt.Fscanln(file, &Stuff) {
 			if Stuff == mobileID {
@@ -232,17 +203,14 @@ func HealMobilesFightCheck(dir, mobileID string) bool {
 		}
 		return nil
 	})
-
 	if err != nil {
 		LogIt("HealMobilesFightCheck - Error walking through files")
 		os.Exit(1)
 	}
-
 	if err := ChgDir(HomeDir); err != nil {
 		LogIt("HealMobilesFightCheck - Change directory to HomeDir failed")
 		os.Exit(1)
 	}
-
 	return MobFighting
 }
 
@@ -250,17 +218,14 @@ func HealMobilesFightCheck(dir, mobileID string) bool {
 func MakeMobilesMove() {
 	RoomMobListFileName := CONTROL_DIR + "RoomMobList.txt"
 	RoomMobMoveFileName := CONTROL_DIR + "RoomMobMove.txt"
-
 	RoomMobListFile, err1 := os.Open(RoomMobListFileName)
 	if err1 == nil {
 		defer RoomMobListFile.Close()
 	}
-
 	RoomMobMoveFile, err2 := os.Open(RoomMobMoveFileName)
 	if err2 == nil {
 		defer RoomMobMoveFile.Close()
 	}
-
 	if err1 == nil {
 		if stat, _ := RoomMobListFile.Stat(); stat.Size() == 0 {
 			RoomMobListFile.Close()
@@ -268,7 +233,6 @@ func MakeMobilesMove() {
 			err1 = os.ErrNotExist
 		}
 	}
-
 	if err2 == nil {
 		if stat, _ := RoomMobMoveFile.Stat(); stat.Size() == 0 {
 			RoomMobMoveFile.Close()
@@ -276,20 +240,16 @@ func MakeMobilesMove() {
 			err2 = os.ErrNotExist
 		}
 	}
-
 	if err2 == nil {
 		MakeMobilesMove3()
 		return
 	}
-
 	if err1 == nil {
 		MakeMobilesMove2()
 		return
 	}
-
 	MakeMobilesMove1()
 }
-
 
 // Build file containing RoomMob file list
 func MakeMobilesMove1() {
@@ -300,12 +260,10 @@ func MakeMobilesMove1() {
 		os.Exit(1)
 	}
 	defer RoomMobListFile.Close()
-
 	if err := ChgDir(ROOM_MOB_DIR); err != nil {
 		LogIt("MakeMobilesMove1 - Change directory to ROOM_MOB_DIR failed")
 		os.Exit(1)
 	}
-
 	RoomMobList := []string{}
 	err = filepath.Walk("./", func(path string, info os.FileInfo, err error) error {
 		if err != nil {
@@ -314,7 +272,6 @@ func MakeMobilesMove1() {
 		if info.IsDir() {
 			return nil
 		}
-
 		RoomMobFileName := info.Name()
 		if !strings.Contains(RoomMobFileName, "Spawn") {
 			TmpStr := fmt.Sprintf("%05d", rand.Intn(100000))
@@ -326,22 +283,18 @@ func MakeMobilesMove1() {
 		}
 		return nil
 	})
-
 	if err != nil {
 		LogIt("MakeMobilesMove1 - Error walking through files")
 		os.Exit(1)
 	}
-
 	sort.Strings(RoomMobList)
 	for _, item := range RoomMobList {
 		TmpStr := strings.Fields(item)[1] + "\n"
 		RoomMobListFile.WriteString(TmpStr)
 	}
-
 	if len(RoomMobList) == 0 {
 		os.Remove(RoomMobListFileName)
 	}
-
 	if err := ChgDir(HomeDir); err != nil {
 		LogIt("MakeMobilesMove1 - Change directory to HomeDir failed")
 		os.Exit(1)
@@ -353,31 +306,26 @@ func MakeMobilesMove2() {
 	RoomMobListFileName := CONTROL_DIR + "RoomMobList.txt"
 	RoomMobListTempFileName := CONTROL_DIR + "RoomMobListTemp.txt"
 	RoomMobMoveFileName := CONTROL_DIR + "RoomMobMove.txt"
-
 	RoomMobListFile, err := os.Open(RoomMobListFileName)
 	if err != nil {
 		LogIt("MakeMobilesMove2 - Open RoomMobList file failed")
 		os.Exit(1)
 	}
 	defer RoomMobListFile.Close()
-
 	RoomMobListTempFile, err := os.Create(RoomMobListTempFileName)
 	if err != nil {
 		LogIt("MakeMobilesMove2 - Create RoomMobListTemp file failed")
 		os.Exit(1)
 	}
 	defer RoomMobListTempFile.Close()
-
 	RoomMobMoveFile, err := os.Create(RoomMobMoveFileName)
 	if err != nil {
 		LogIt("MakeMobilesMove2 - Create RoomMobMove file failed")
 		os.Exit(1)
 	}
 	defer RoomMobMoveFile.Close()
-
 	TimerStart := time.Now()
 	TimerStop := TimerStart.Add(100 * time.Millisecond)
-
 	Scanner := bufio.NewScanner(RoomMobListFile)
 	for Scanner.Scan() {
 		RoomMobFileName := Scanner.Text()
@@ -385,7 +333,6 @@ func MakeMobilesMove2() {
 			RoomMobListTempFile.WriteString(RoomMobFileName + "\n")
 			continue
 		}
-
 		RoomID = strings.TrimSuffix(RoomMobFileName, filepath.Ext(RoomMobFileName))
 		RoomMobFilePath := ROOM_MOB_DIR + RoomMobFileName
 		RoomMobFile, err := os.Open(RoomMobFilePath)
@@ -393,24 +340,20 @@ func MakeMobilesMove2() {
 			continue
 		}
 		defer RoomMobFile.Close()
-
 		RoomScanner := bufio.NewScanner(RoomMobFile)
 		for RoomScanner.Scan() {
 			Stuff = RoomScanner.Text()
 			// Placeholder for processing each mobile in the room
 		}
 	}
-
 	if err := Scanner.Err(); err != nil {
 		LogIt("MakeMobilesMove2 - Error scanning RoomMobList file")
 		os.Exit(1)
 	}
-
 	if err := os.Remove(RoomMobListFileName); err != nil {
 		LogIt("MakeMobilesMove2 - Remove RoomMobList file failed")
 		os.Exit(1)
 	}
-
 	if err := os.Rename(RoomMobListTempFileName, RoomMobListFileName); err != nil {
 		LogIt("MakeMobilesMove2 - Rename RoomMobListTemp file failed")
 		os.Exit(1)
@@ -425,7 +368,6 @@ func MakeMobilesMove3() {
 		RoomMobMoveFileName, RoomMobMoveTempFileName             string
 		TimerStart, TimerStop                                   time.Time
 	)
-
 	MobMoveNotCompleted = false
 	RoomMobMoveFileName = CONTROL_DIR + "RoomMobMove.txt"
 	RoomMobMoveFile, err := os.Open(RoomMobMoveFileName)
@@ -434,7 +376,6 @@ func MakeMobilesMove3() {
 		return
 	}
 	defer RoomMobMoveFile.Close()
-
 	RoomMobMoveTempFileName = CONTROL_DIR + "RoomMobMoveTemp.txt"
 	RoomMobMoveTempFile, err := os.Create(RoomMobMoveTempFileName)
 	if err != nil {
@@ -442,10 +383,8 @@ func MakeMobilesMove3() {
 		return
 	}
 	defer RoomMobMoveTempFile.Close()
-
 	TimerStart = time.Now()
 	TimerStop = TimerStart.Add(100 * time.Millisecond)
-
 	Scanner := bufio.NewScanner(RoomMobMoveFile)
 	for Scanner.Scan() {
 		Stuff := Scanner.Text()
@@ -454,14 +393,12 @@ func MakeMobilesMove3() {
 			RoomMobMoveTempFile.WriteString(Stuff + "\n")
 			continue
 		}
-
 		MobileId = StrGetWord(Stuff, 1)
 		RoomId = StrGetWord(Stuff, 2)
 		ExitToRoomId = StrGetWord(Stuff, 3)
 		if !IsMobileIdInRoom(RoomId, MobileId) {
 			continue
 		}
-
 		MobileDesc1 = GetMobDesc1(MobileId)
 		LeaveMsg = MobileDesc1 + " leaves."
 		ArriveMsg = MobileDesc1 + " arrives."
@@ -469,7 +406,6 @@ func MakeMobilesMove3() {
 		AddMobToRoom(ExitToRoomId, MobileId)
 		SendToRoom(RoomId, LeaveMsg)
 		SendToRoom(ExitToRoomId, ArriveMsg)
-
 		PositionOfDot := strings.Index(MobileId, ".")
 		if PositionOfDot > 1 {
 			MobStatsFileName := MOB_STATS_ROOM_DIR + MobileId + ".txt"
@@ -480,17 +416,14 @@ func MakeMobilesMove3() {
 			CreateMobStatsFileWrite(MOB_STATS_ROOM_DIR, MobileId, ExitToRoomId)
 		}
 	}
-
 	if err := Scanner.Err(); err != nil {
 		LogIt("MakeMobilesMove3 - Error scanning RoomMobMove file")
 		return
 	}
-
 	if err := os.Remove(RoomMobMoveFileName); err != nil {
 		LogIt("MakeMobilesMove3 - Remove RoomMobMove file failed")
 		return
 	}
-
 	if MobMoveNotCompleted {
 		if err := os.Rename(RoomMobMoveTempFileName, RoomMobMoveFileName); err != nil {
 			LogIt("MakeMobilesMove3 - Rename RoomMobMoveTemp file failed")
@@ -521,15 +454,12 @@ func SpawnMobile(MobileId, RoomId string) {
 		LogIt(LogMessage)
 		return
 	}
-
 	AddMobToRoom(RoomId, MobileId)
 	SpawnMsg = pMobile.Desc1 + " suddenly appears!"
 	SendToRoom(RoomId, SpawnMsg)
 	MobileAction = pMobile.Action
-
 	// Clean up
 	pMobile = nil
-
 	if StrIsWord("NoMove", MobileAction) {
 		SpawnMobileNoMove(MobileId)
 	}
