f24515d4dcb171c5915f2d0c27743f996997b856
Khorlane <khorlane@yahoo.com>
2025-12-11 08:00:50 -0500

    Convert tabs to spaces in Help.go

--- Source/Server/Help.go
+++ Source/Server/Help.go
@@ -10,8 +10,8 @@
 package server
 
 import (
-	"bufio"
-	"os"
+  "bufio"
+  "os"
 )
 
 var HelpFile *os.File
@@ -19,67 +19,67 @@ var HelpScanner *bufio.Scanner
 var HelpText string
 
 func IsHelp() bool {
-	DEBUGIT(1)
-	Found := false
-	if !OpenHelpFile() {
-		return false
-	}
-	HelpLookup := StrGetWord(CmdStr, 2)
-	HelpLookup = StrMakeLower(HelpLookup)
-	HelpText = "Not Done"
-	for HelpText != "End of Help" {
-		HelpReadLine()
-		HelpText = StrTrimLeft(HelpText)
-		TmpStr = StrLeft(HelpText, 5)
-		if TmpStr == "Help:" {
-			TmpStr = StrRight(HelpText, StrGetLength(HelpText)-5)
-			TmpStr = StrMakeLower(TmpStr)
-			if TmpStr == HelpLookup {
-				Found = true
-				ShowHelp()
-				HelpText = "End of Help"
-			}
-		}
-	}
-	if HelpFile != nil {
-		HelpFile.Close()
-	}
-	if Found {
-		return true
-	}
-	return false
+  DEBUGIT(1)
+  Found := false
+  if !OpenHelpFile() {
+    return false
+  }
+  HelpLookup := StrGetWord(CmdStr, 2)
+  HelpLookup = StrMakeLower(HelpLookup)
+  HelpText = "Not Done"
+  for HelpText != "End of Help" {
+    HelpReadLine()
+    HelpText = StrTrimLeft(HelpText)
+    TmpStr = StrLeft(HelpText, 5)
+    if TmpStr == "Help:" {
+      TmpStr = StrRight(HelpText, StrGetLength(HelpText)-5)
+      TmpStr = StrMakeLower(TmpStr)
+      if TmpStr == HelpLookup {
+        Found = true
+        ShowHelp()
+        HelpText = "End of Help"
+      }
+    }
+  }
+  if HelpFile != nil {
+    HelpFile.Close()
+  }
+  if Found {
+    return true
+  }
+  return false
 }
 
 func OpenHelpFile() bool {
-	DEBUGIT(1)
-	HelpFileName := HELP_DIR + "Help.txt"
-	file, err := os.Open(HelpFileName)
-	if err != nil {
-		return false
-	}
-	HelpFile = file
-	HelpScanner = bufio.NewScanner(HelpFile)
-	return true
+  DEBUGIT(1)
+  HelpFileName := HELP_DIR + "Help.txt"
+  file, err := os.Open(HelpFileName)
+  if err != nil {
+    return false
+  }
+  HelpFile = file
+  HelpScanner = bufio.NewScanner(HelpFile)
+  return true
 }
 
 func HelpReadLine() {
-	DEBUGIT(1)
-	if HelpScanner != nil && HelpScanner.Scan() {
-		HelpText = HelpScanner.Text()
-	} else {
-		HelpText = ""
-	}
+  DEBUGIT(1)
+  if HelpScanner != nil && HelpScanner.Scan() {
+    HelpText = HelpScanner.Text()
+  } else {
+    HelpText = ""
+  }
 }
 
 func ShowHelp() {
-	TmpStr = StrLeft(HelpText, 13)
-	for TmpStr != "Related help:" {
-		HelpReadLine()
-		pDnodeActor.PlayerOut += HelpText
-		pDnodeActor.PlayerOut += "\r\n"
-		TmpStr = StrLeft(HelpText, 13)
-	}
-	pDnodeActor.PlayerOut += "\r\n"
-	CreatePrompt(pDnodeActor.pPlayer)
-	pDnodeActor.PlayerOut += GetPlayerOutput(pDnodeActor.pPlayer)
+  TmpStr = StrLeft(HelpText, 13)
+  for TmpStr != "Related help:" {
+    HelpReadLine()
+    pDnodeActor.PlayerOut += HelpText
+    pDnodeActor.PlayerOut += "\r\n"
+    TmpStr = StrLeft(HelpText, 13)
+  }
+  pDnodeActor.PlayerOut += "\r\n"
+  CreatePrompt(pDnodeActor.pPlayer)
+  pDnodeActor.PlayerOut += GetPlayerOutput(pDnodeActor.pPlayer)
 }
